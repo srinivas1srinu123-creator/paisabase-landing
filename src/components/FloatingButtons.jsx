e763d751f13a5fd88168b3d88ded2d0e0c445e06
srinivas1srinu123-creator <srinivas1srinu123@gmail.com>
2026-06-23 18:04:18 +0530

    Update FloatingButtons.jsx

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -1,27 +1,13 @@
 export default function FloatingButtons() {
   return (
-    <div className="fixed bottom-8 right-6 flex flex-col gap-4 z-50">
-
-      {/* Telegram */}
-      <a
-        href="https://t.me/Paisabaseofficial"
-        target="_blank"
-        rel="noreferrer"
-        className="shadow-lg hover:scale-110 transition"
-      >
-        <img src="/telegram.png" alt="Telegram" className="w-14 h-14" />
+    <div className="fixed bottom-6 right-3 md:right-6 flex flex-col gap-3 md:gap-4 z-50">
+      <a href="https://t.me/Paisabaseofficial" target="_blank" rel="noreferrer">
+        <img src="/telegram.png" alt="Telegram" className="w-12 h-12 md:w-14 md:h-14" />
       </a>
 
-      {/* WhatsApp */}
-      <a
-        href="https://wa.me/919236243851?text=Hi%20I%27m%20from%20PaisaBASE,%20If%20you%20want%20to%20know%20more%20about%20INR%20purchase%20and%20USDT%20trading%20contact%20me."
-        target="_blank"
-        rel="noreferrer"
-        className="shadow-lg hover:scale-110 transition"
-      >
-        <img src="/whatsapp.png" alt="WhatsApp" className="w-14 h-14" />
+      <a href="https://wa.me/919236243851" target="_blank" rel="noreferrer">
+        <img src="/whatsapp.png" alt="WhatsApp" className="w-12 h-12 md:w-14 md:h-14" />
       </a>
-
     </div>
   );
 }
